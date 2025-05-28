80c3e3f65fd5c78f161de8168d3b759267adc41b
xiazhiqiang14 <xiazhiqiang14@jd.com>
2025-05-28 15:13:10 +0800

    chore: 修改编译配置

--- src/app.ts
+++ src/app.ts
@@ -10,29 +10,29 @@ const app = express();
 
 
 async function startServer() {
-try {
-    // 初始化数据库
-    await initializeDatabase();
-    
-    // 中间件
-    app.use(express.json());
-    app.use(dbMiddleware); // 注入数据库实例
-    
-    // 错误处理中间件
-    app.use(errorHandler);
+    try {
+        // 初始化数据库
+        await initializeDatabase();
+        debugger
+        // 中间件
+        app.use(express.json());
+        app.use(dbMiddleware); // 注入数据库实例
+        
+        // 错误处理中间件
+        app.use(errorHandler);
 
-    // 注入路由
-    app.use('/scraper', setRoutes);
-    
-    // 启动服务器
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
-} catch (error) {
-    console.error('Failed to start server:', error);
-    process.exit(1);
-}
+        // 注入路由
+        app.use('/scraper', setRoutes);
+        
+        // 启动服务器
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
 }
 
 startServer();
